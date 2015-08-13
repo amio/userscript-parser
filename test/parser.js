1d47be39be770d76e452c5ee9debd6ae3b3dc597
Amio <amio.cn@gmail.com>
2015-08-13 20:54:18 +0800

    (test) Update tests.

--- test/parser.js
+++ test/parser.js
@@ -8,8 +8,39 @@ tape.test('parser', function (t) {
   //
   t.equal(typeof usp, 'function', 'require("userscript") should be a function')
 
-  const result = usp(validUserscript)
-  console.log(result)
+  const parsed = usp(validUserscript)
+  const expected = {
+    description: [
+      'This script even does the laundry!',
+      'Adds a button that lets you download YouTube videos.'
+    ],
+    downloadURL: [ 'https://www.example.com/myscript.user.js' ],
+    name: [ 'Download YouTube Videos as MP4' ],
+    homepageURL: [ 'https://github.com/gantt/downloadyoutube' ],
+    author: [ 'Gantt' ],
+    version: [ '1.8.3' ],
+    date: [ '2015-05-17' ],
+    namespace: [ 'http://googlesystem.blogspot.com' ],
+    include: [ 'http://www.youtube.com/*', 'https://www.youtube.com/*' ],
+    exclude: [
+      'http://www.youtube.com/embed/*',
+      'https://www.youtube.com/embed/*' ],
+    match: [
+      'http://www.youtube.com/*',
+      'https://www.youtube.com/*',
+      'http://s.ytimg.com/yts/jsbin/html5player*',
+      'https://s.ytimg.com/yts/jsbin/html5player*',
+      'http://manifest.googlevideo.com/*',
+      'https://manifest.googlevideo.com/*',
+      'http://*.googlevideo.com/videoplayback*',
+      'https://*.googlevideo.com/videoplayback*',
+      'http://*.youtube.com/videoplayback*',
+      'https://*.youtube.com/videoplayback*' ],
+    grant: [ 'GM_xmlhttpRequest', 'GM_getValue', 'GM_setValue' ],
+    license: [ 'MIT License' ],
+    content: "\n\nvar mynameis = 'USERSCRIPT!'\n"
+  }
+  t.deepEqual(parsed, expected)
 
   t.end()
 })
